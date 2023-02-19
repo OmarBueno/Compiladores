1a3d90a31ffce0a9d3dbcf9fbe172ac906bb5701
OmarBueno <omarzaldivar07@aragon.unam.mx>
2023-02-19 14:23:10 -0600

    Se agrego configuracion y data para programa 5

--- Programas/LexicoUno/src/fes/aragon/test/AFDConfig.java
+++ Programas/LexicoUno/src/fes/aragon/test/AFDConfig.java
@@ -6,9 +6,9 @@ import fes.aragon.util.Archivo;
 public class AFDConfig {
 	public static void main(String[] args) {
 		AFD automata = new AFD();
-		automata.configurar("Config3");
+		automata.configurar("Config4");
 		// automata.evaluar("aabbcca:");
-		for (String token : Archivo.getData("Data2")) {
+		for (String token : Archivo.getData("Data4")) {
 			System.out.println(automata.evaluar(token));
 		}
 	}
